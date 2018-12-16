42b26fcff7d83a300684c45b5e6e015124afb695
sdaliri <sdaliri4@gmail.com>
2018-12-16 11:58:14 +0330

    #2 semantic is added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//ُSemantic Library
+import SuiVue from 'semantic-ui-vue'
+import 'semantic-ui-css/semantic.min.css' 
+
+
+Vue.use(SuiVue);
+
 
 Vue.config.productionTip = false
 
