00ed5e56d5a0df85caf5baf93e3cd3491721d5b3
arshivbastian <37534259+arshivbastian@users.noreply.github.com>
2018-12-17 10:21:55 +0330

    #2 #3 add JQuery
    necessity

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//jquery global
+import 'jquery'
 //ُSemantic Library
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css' 
@@ -17,6 +19,7 @@ Vue.use(SuiVue);
 Vue.config.productionTip = false
 
 new Vue({
+  Vue,
   router,
   store,
   render: h => h(App)
