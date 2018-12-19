44ff49ddf79bcc19fb7ffcd7193e2b5a192da8a8
sdaliri <sdaliri4@gmail.com>
2018-12-19 11:01:51 +0330

    #2  add portal-vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 //jquery global
 import 'jquery'
 //ُSemantic Library
+import PortalVue from 'portal-vue'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css' 
 //Bootstrap Libarary
@@ -12,6 +13,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(PortalVue);
 Vue.use(BootstrapVue);
 Vue.use(SuiVue);
 
